8f46cbbc816aaca938c63dcd418f43d2a571d37d
Stela Slavcheva <slavcheva2711@gmail.com>
2022-03-22 10:38:42 +0200

    added modal window

---
+++ src/views/modal.js
@@ -0,0 +1,25 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js'
+
+const modalTemplate = (msg, onChoice) => html `
+  <div id="modal" >
+        <p>${msg}</p>
+        <button @click=${() => onChoice(true)}>OK</button>
+        <button @click=${() => onChoice(false)}>Cancel</button>
+      </div >
+`;
+const overlay = document.createElement('div');
+overlay.id = 'overlay';
+
+export function createModal(msg, callback){
+    render(modalTemplate(msg, onChoice), overlay);
+    document.body.appendChild(overlay);
+
+    function onChoice(choice){
+        clearModal();
+              
+        callback(choice);
+    }
+}
+function clearModal(){
+    overlay.remove();
+}
